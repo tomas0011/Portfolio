43a45f6b43ccb6835f82ab11096ffb87f746961d
Cristian Juarez <cristianjuarezdev@gmail.com>
2023-07-28 23:08:36 -0300

    Footer layout and responsiveness

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,9 +3,32 @@ import './Footer.css';
 export const Footer = () => {
   return (
     <footer className="Footer">
+      <h5>"Ningún mar en calma hizo experto a un marinero"</h5>
       <div>
-        <h3>Redes sociales</h3>
+        <div>
+          <h3>Contáctame</h3>
+          <span>linkedin.com/in/tomasbvasquez</span>
+          <span>linkedin.com/in/tomasbvasquez</span>
+          <span>linkedin.com/in/tomasbvasquez</span>
+          <span>linkedin.com/in/tomasbvasquez</span>
+          <br/>
+          <span>(botones como los del cv)</span>
+        </div>
+        <div>
+          <h3>Perfil</h3>
+          <span>Soy una persona comprometida con lo que hace, un amante de la tecnologia e innovacion. Me gusta implementar código de calidad y soy autoexigente con eso, adapto las nuevas cosas que aprendo a los proyectos en los que trabajo, ya que la mejor forma de aprender es llevándolas a la práctica.</span>
+        </div>
+        <div>
+          <h3>Sobre mi</h3>
+          <h5>FECHA DE NACIMIENTO</h5>
+          <span>20/11/2001</span>
+          <h5>NACIONALIDAD</h5>
+          <span>Argentino</span>
+          <h5>DOMICILIO</h5>
+          <span>Hurlingham, Buenos Aires, Argentina</span>
+        </div>
       </div>
+      <span>Copyright © - Tomas Benjamin Vasquez & <a href="" target='_blank'>Cristian Juarez</a></span>
     </footer>
   );
 }
